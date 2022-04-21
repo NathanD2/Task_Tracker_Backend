fd0c3d86acb2a448ff4ae541fd2b486762d92a8e
Nathan <ndong5@my.bcit.ca>
2022-04-21 14:19:34 -0700

    config changes

--- config.js
+++ config.js
@@ -5,26 +5,26 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-// const pool = new Pool({
-//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-//   ssl: {
-//     rejectUnauthorized: false
-//     },
-// })
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false
+    },
+})
 
-const pool = (() => {
-  if (process.env.NODE_ENV !== 'production') {
-      return new Pool({
-          connectionString: connectionString,
-          ssl: false
-      });
-  } else {
-      return new Pool({
-          connectionString: connectionString,
-          ssl: {
-              rejectUnauthorized: false
-            }
-      });
-  } })();
+// const pool = (() => {
+//   if (process.env.NODE_ENV !== 'production') {
+//       return new Pool({
+//           connectionString: connectionString,
+//           ssl: false
+//       });
+//   } else {
+//       return new Pool({
+//           connectionString: connectionString,
+//           ssl: {
+//               rejectUnauthorized: false
+//             }
+//       });
+//   } })();
 
 module.exports = { pool }
