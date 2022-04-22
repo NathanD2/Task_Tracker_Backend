bf1dde956079fbf577d28fc111c2b5c4fc83adcf
Nathan <ndong5@my.bcit.ca>
2022-04-21 18:01:23 -0700

    added new endpoints

--- server.js
+++ server.js
@@ -16,44 +16,10 @@ app.use(express.urlencoded({ extended: true }))
 // app.use(cors(origin))
 app.use(cors());
 
-const getTasks = (request, response) => {
-  pool.query('SELECT * FROM tasks', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const addTask = (request, response) => {
-  const { task, date_time, reminder } = request.body
-
-  pool.query(
-    'INSERT INTO tasks (task, date_time, reminder) VALUES ($1, $2, $3)',
-    [task, date_time, reminder],
-    (error) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({ status: 'success', message: 'Task added.' })
-    }
-  )
-}
-
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
-app.get('/tasks', getTasks);
-app.post('/tasks', addTask);
-
-// app
-//   .route('/tasks')
-//   // GET endpoint
-//   .get(getTasks)
-//   // POST endpoint
-//   .post(addTask)
+// Version 1
+require('./version/v1')(app, pool);
 
 // Start server
 app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening at http://localhost:3002/`)
+  console.log(`Server listening at http://localhost:${process.env.PORT || 3002}/`)
 })
---
+++ version/v1.js
@@ -0,0 +1,128 @@
+module.exports = (app, pool) => {
+  
+  // ======================
+  // Get Requests
+  // ======================
+
+  // Gets all tasks
+  const getTasks = (request, response) => {
+    pool.query("SELECT * FROM tasks", (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    });
+  };
+
+  // Gets single task by ID
+  const getTask = (req, res) => {
+    const taskID = req.params.taskId;
+    pool.query("SELECT * FROM tasks where id = $1",
+    [taskID],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    });
+  }
+
+  // ======================
+  // Post Requests
+  // ======================
+
+  // Add a new task
+  const addTask = (request, response) => {
+    const { task, date_time, reminder } = request.body;
+
+    pool.query(
+      "INSERT INTO tasks (task, date_time, reminder) VALUES ($1, $2, $3)",
+      [task, date_time, reminder],
+      (error) => {
+        if (error) {
+          throw error;
+        }
+        response
+          .status(201)
+          .json({ status: "success", message: "Task added." });
+      }
+    );
+  };
+
+  // ======================
+  // Delete Requests
+  // ======================
+
+  // Deletes a task by id included in request body.
+  const deleteTask = (req, res) => {
+    const { id } = req.body;
+    pool.query(
+      "DELETE FROM tasks WHERE id = $1",
+      [id],
+      (error) => {
+        if (error) {
+          throw error;
+        }
+        res
+          .status(201)
+          .json({ status: "success", message: "Task Successfully Deleted." });
+      }
+    );
+  }
+
+  // ======================
+  // Put Requests
+  // ======================
+
+  // Updates Reminder for a task by id
+  const setReminder = (req, res) => {
+    const { id, reminder } = req.body;
+
+    pool.query(
+      "UPDATE tasks SET reminder = $2 WHERE id = $1",
+      [id, reminder],
+      (error) => {
+        if (error) {
+          throw error;
+        }
+        res
+          .status(201)
+          .json({ status: "success", message: "Task Reminder Updated." });
+      }
+    );
+  };
+
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+  // Wake up heroku server
+  app.get("/wakeup", (req, res) => {
+    res.send("Im awake!");
+  });
+
+  // ======================
+  // Get Requests
+  // ======================
+
+  app.get("/tasks", getTasks);
+  app.get("/task/:taskId", getTask);
+
+  // ======================
+  // Post Requests
+  // ======================
+
+  app.post("/addtask", addTask);
+
+  // ======================
+  // Delete Requests
+  // ======================
+
+  app.delete("/delete", deleteTask);
+
+  // ======================
+  // Put Requests
+  // ======================
+  
+  app.put("/setreminder", setReminder);
+};
